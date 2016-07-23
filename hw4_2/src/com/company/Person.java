8c8233dd152affc918988b229fc260ea4682c713
poc.hsu <poc7667@gmail.com>
2016-07-23 11:20:37 -0700

    add Person class

---
+++ hw4_2/src/com/company/Person.java
@@ -0,0 +1,46 @@
+package com.company;
+
+/**
+ * Created by poc on 7/23/16.
+ */
+public abstract class Person {
+
+    private String firstName ="";
+    private String lastName ="";
+    private String email="";
+
+    public Person(){}
+
+    @Override
+    public String toString(){
+        System.out.printf("Name: %s %s\n", firstName, lastName);
+        System.out.printf("Email: %s \n", email);
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract String getDisplayText();
+}
